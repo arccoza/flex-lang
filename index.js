776c665566ad53693160ed56c64661e207e64eba
Adrien <adrien@arccoza.com>
2017-07-15 03:38:58 +0200

    Refactor parser to use regex -> gen mappings

--- index.js
+++ index.js
@@ -43,51 +43,62 @@ class FlexLang {
     var reDistribution = /(?:([JAS]{1,3})(\[[-~\s]*\]))/g
     var direction, wrapping, distributions
 
-    distributions = str
-      .replace(reDirection, (r, t) => (print(r), direction = t, ''))
-      .replace(reWrapping, (r, t) => (print(r), wrapping = t, ''))
+    // distributions = str
+    //   .replace(reDirection, (r, t) => (print(r), direction = t, ''))
+    //   .replace(reWrapping, (r, t) => (print(r), wrapping = t, ''))
 
-    if (direction)
-      style[tokens['property'][format]['direction']] = tokens['direction'][direction]
-    if (wrapping)
-      style[tokens['property'][format]['wrapping']] = tokens['wrapping'][wrapping]
-
-    // m: match, p: properties, d: distribution, s: stretch
-    for (let m, p, d, s; m = reDistribution.exec(distributions);) {
-      // Destructure match into properties and distribution.
-      [, p, d] = m
-      // Check for stretch mode,
-      // replace stretch symbol with normal dist symbol `-`,
-      // so we can do a lookup in `tokens`.
-      d = d.replace(/~/g, r => (s = true, '-'))
-
-      print(p, d)
-      let val = null
-      for (let k of [...p]) {
-        print(k)
-        let prop = tokens['property'][format][k]
-        // let val = null
-
-        print(val)
-        if (k != 'A')
-          val = val || tokens['distribution'][d]
-        else
-          val = tokens['alignment'][d]
-
-        if (s && (k != 'A')) {
-          if (k == 'C')
-            style[prop] = 'stretch'
-          else {
-            style[prop] = val
-            childStyle['flex'] = '1'
-          }
-        }
-        else
-          style[prop] = val
-
-        print(prop, val)
+    for (let [re, fn] of this.reMap) {
+      for (let m of hunt(re, str)) {
+        for (let [k, v] of fn(m))
+          style[k] = v
+          // print(k, v)
+        // let [k, v] = fn(m)
+        // style[k] = v
+        // print(fn(m))
       }
     }
+
+    // if (direction)
+    //   style[tokens['property'][format]['direction']] = tokens['direction'][direction]
+    // if (wrapping)
+    //   style[tokens['property'][format]['wrapping']] = tokens['wrapping'][wrapping]
+
+    // // m: match, p: properties, d: distribution, s: stretch
+    // for (let m, p, d, s; m = reDistribution.exec(distributions);) {
+    //   // Destructure match into properties and distribution.
+    //   [, p, d] = m
+    //   // Check for stretch mode,
+    //   // replace stretch symbol with normal dist symbol `-`,
+    //   // so we can do a lookup in `tokens`.
+    //   d = d.replace(/~/g, r => (s = true, '-'))
+
+    //   print(p, d)
+    //   let val = null
+    //   for (let k of [...p]) {
+    //     print(k)
+    //     let prop = tokens['property'][format][k]
+    //     // let val = null
+
+    //     print(val)
+    //     if (k != 'A')
+    //       val = val || tokens['distribution'][d]
+    //     else
+    //       val = tokens['alignment'][d]
+
+    //     if (s && (k != 'A')) {
+    //       if (k == 'C')
+    //         style[prop] = 'stretch'
+    //       else {
+    //         style[prop] = val
+    //         childStyle['flex'] = '1'
+    //       }
+    //     }
+    //     else
+    //       style[prop] = val
+
+    //     print(prop, val)
+    //   }
+    // }
 
     return [style, childStyle]
   }
@@ -98,4 +109,4 @@ class FlexLang {
 }
 
 var fl = new FlexLang({format: 'css'})
-print(...fl.layout('-H, </>, J[-- ], JAS[ ~ ~ ]'))
+print(...fl.layout('-H, </>, J[-- ], JAS[ -  - ]'))
