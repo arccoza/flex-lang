f19bc4235f939d7b39b381cd98f4e7efc175cd14
Adrien <adrien@arccoza.com>
2017-07-15 04:20:32 +0200

    Tidy up, remove old or commented code

--- index.js
+++ index.js
@@ -16,23 +16,6 @@ function* hunt(re, str) {
 class FlexLang {
   constructor({format = 'js'} = {}) {
     this.format = format
-    this.reMap = [
-      [/(-?H|-?V)(?:,\s*)|$/, function*([, m]) {
-        yield [tokens['property'][format]['direction'], tokens['direction'][m]]
-      }],
-
-      [/([<>][/\s]?[<>])(?:,\s*)|$/, function*([, m]) {
-        yield [tokens['property'][format]['wrapping'], tokens['wrapping'][m]]
-      }],
-
-      [/(?:([JAS]{1,3})(\[[-~\s]*\]))/g, function*([, p, d]) {
-        let v = tokens['distribution'][d]
-        for (let k of [...p]) {
-          yield [tokens['property'][format][k], v]
-        }
-      }],
-    ]
-
     this.reMap2 = [
       ['direction', /(-?H|-?V)(?:,\s*)|$/],
       ['wrapping', /([<>][/\s]?[<>])(?:,\s*)|$/],
@@ -44,22 +27,6 @@ class FlexLang {
     var format = this.format
     var style = {}
     var childStyle = {}
-    var reDirection = /(-?H|-?V)(?:,\s*)|$/
-    var reWrapping = /([<>/\s]{2,3})(?:,\s*)|$/
-    var reDistribution = /(?:([JAS]{1,3})(\[[-~\s]*\]))/g
-    var direction, wrapping, distributions
-
-    // distributions = str
-    //   .replace(reDirection, (r, t) => (print(r), direction = t, ''))
-    //   .replace(reWrapping, (r, t) => (print(r), wrapping = t, ''))
-
-    // for (let [re, fn] of this.reMap) {
-    //   for (let m of hunt(re, str)) {
-    //     for (let [k, v] of fn(m))
-    //       style[k] = v
-    //       // print(k, v)
-    //   }
-    // }
 
     for (let [nm, re] of this.reMap2) {
       for (let m of hunt(re, str)) {
@@ -78,48 +45,6 @@ class FlexLang {
         }
       }
     }
-
-    // if (direction)
-    //   style[tokens['property'][format]['direction']] = tokens['direction'][direction]
-    // if (wrapping)
-    //   style[tokens['property'][format]['wrapping']] = tokens['wrapping'][wrapping]
-
-    // // m: match, p: properties, d: distribution, s: stretch
-    // for (let m, p, d, s; m = reDistribution.exec(distributions);) {
-    //   // Destructure match into properties and distribution.
-    //   [, p, d] = m
-    //   // Check for stretch mode,
-    //   // replace stretch symbol with normal dist symbol `-`,
-    //   // so we can do a lookup in `tokens`.
-    //   d = d.replace(/~/g, r => (s = true, '-'))
-
-    //   print(p, d)
-    //   let val = null
-    //   for (let k of [...p]) {
-    //     print(k)
-    //     let prop = tokens['property'][format][k]
-    //     // let val = null
-
-    //     print(val)
-    //     if (k != 'A')
-    //       val = val || tokens['distribution'][d]
-    //     else
-    //       val = tokens['alignment'][d]
-
-    //     if (s && (k != 'A')) {
-    //       if (k == 'C')
-    //         style[prop] = 'stretch'
-    //       else {
-    //         style[prop] = val
-    //         childStyle['flex'] = '1'
-    //       }
-    //     }
-    //     else
-    //       style[prop] = val
-
-    //     print(prop, val)
-    //   }
-    // }
 
     return [style, childStyle]
   }
